a5cd4db92e11ab5eb01a264d500e3a16b11ae9bc
Tech-Hiring-24-Technology <sonarsuresh1299@gmail.com>
2024-06-08 12:43:38 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,1 +1,24 @@
+<?php
+$name= $_POST['name'];
+$email= $_POST['email'];
+$contact= $_POST['contact'];
+$addres= $_POST['address'];
+
+
+
+
+$conn = new mysqli('localhost', 'root','', 'test');
+if($conn->connect_error) {
+die('Connection Failed : $conn->connect_error);
+}else{
+$stmt $conn->prepare("insert into profile (name, email, contact, address)
+values(?, ?, ?, ?)");
+$stmt->bind_param("ssis", $name, $email, $contact, $address);
+$stmt->execute();
+echo "registration Successfully...";
+$stmt->close();
+$conn->close();
+}
+?>
+
 
